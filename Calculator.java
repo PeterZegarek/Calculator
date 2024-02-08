666d6b7a6e21806ba10ec7d25b2b445c5f4edc88
PeterZegarek <petrzegarek@gmail.com>
2024-02-07 19:19:34 -0500

    Step 1 commit

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Calculator{
     public static void main(String args[]){
-        System.out.println("Hello world!!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please input 2 whole numbers. Type the first number, hit enter, type the second number, hit enter.");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        
+
+        sc.close();
     }
 }
