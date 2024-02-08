d9bb1410742058302633384b88e56a5780909158
PeterZegarek <petrzegarek@gmail.com>
2024-02-07 19:39:11 -0500

    Step 4 - added support for floating point numbers in the form of replacing ints with doubles. User can enter whole numbers or floating point numbers

--- Calculator.java
+++ Calculator.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String args[]) {
-        // step 1
+        // step 1 - updated to include step 4
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please input 2 whole numbers. Type the first number, hit enter, type the second number, hit enter.");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        System.out.println("Please input 2 whole or floating point numbers. Type the first number, hit enter, type the second number, hit enter.");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
 
-        // step 2 - updated to include step 3
+        // step 2 - updated to include step 3 and 4
         boolean looping = true;
         while (looping) {
             System.out.println("Would you like to perform addition, subtraction, multiplication, or division with those numbers? 'A' for addition, 'S' for subtraction, 'M' for multiplication, 'D' for division.");
@@ -26,7 +26,7 @@ public class Calculator {
                 looping = false;
             }
             else if (next.equals("D") || next.equals("d")) {
-                System.out.println((double)a / b);
+                System.out.println(a / b);
                 looping = false;
             }
         }
